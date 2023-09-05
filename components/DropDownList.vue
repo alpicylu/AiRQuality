eeb9637f8c1121e742672bfb76dc9aaa9c62c60b
Aleksander Łuczycki <aleksander.luczycki@op.pl>
2023-09-05 22:17:53 +0200

    before ubuntu uninstal

---
+++ components/DropDownList.vue
@@ -0,0 +1,23 @@
+<template>
+    <form>
+        <label for="name.toLowerCase()">{{ name }}</label>
+        <select name="name.toLowerCase()" id="name.toLowerCase()">
+            <option v-for="opt in options"
+                :key="opt"
+                :value="opt.toLowerCase()">{{ opt }}
+            </option>
+        </select>
+    </form>
+</template>
+
+<script setup lang="ts">
+
+let x: number;
+
+const props = defineProps<{
+    modelValue: string, //v-model prop
+    name: string,
+    options: string[]
+}>()
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="w-screen h-screen flex flex-row justify-around bg-gray-200">
+    <div class="w-5/6 bg-gray-50">
+        <header>
+            <nav>
+            <div class="flex justify-evenly items-start mt-2">
+                <NuxtLink to="/">Home</NuxtLink>
+                <NuxtLink>Settings</NuxtLink>
+                <NuxtLink>Log In</NuxtLink>
+                <NuxtLink>Help</NuxtLink>
+                <NuxtLink>About</NuxtLink>
+            </div>
+            </nav>
+        </header>
+        <body>
+            <div class="flex justify-end gap-x-5 bg-gray-300 rounded-full w-10/12 h-12 mx-auto my-10">
+                <DropDownList :name="ddName" :options="dropElems"/>
+            </div>
+        </body>    
+    </div>
+    </div>
+</template>
+
+<script setup>
+
+const ddName = "Display";
+
+const dropElems = ref([
+    "Option 1",
+    "Option 2",
+    "Option 3",
+])
+
+</script>
