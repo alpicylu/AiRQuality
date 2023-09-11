c1c04b3aef4ac5610f0127eb4ef7e0fb875dc7ef
Aleksander Łuczycki <aleksander.luczycki@op.pl>
2023-09-11 15:41:28 +0200

    before flexing default layout

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-screen h-screen flex flex-row justify-around bg-ext-margins">
+        <div class="w-5/6 h-full bg-ext-content">
+            <header>
+                <nav>
+                <div class="flex justify-evenly items-start mt-2">
+                    <NuxtLink to="/">Home</NuxtLink>
+                    <NuxtLink>Settings</NuxtLink>
+                    <NuxtLink>Log In</NuxtLink>
+                    <NuxtLink>Help</NuxtLink>
+                    <NuxtLink>About</NuxtLink>
+                </div>
+                </nav>
+            </header>
+            <div>
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,21 @@
 <template>
-    <div class="w-screen h-screen flex flex-row justify-around bg-gray-200">
-    <div class="w-5/6 bg-gray-50">
-        <header>
-            <nav>
-            <div class="flex justify-evenly items-start mt-2">
-                <NuxtLink to="/">Home</NuxtLink>
-                <NuxtLink>Settings</NuxtLink>
-                <NuxtLink>Log In</NuxtLink>
-                <NuxtLink>Help</NuxtLink>
-                <NuxtLink>About</NuxtLink>
-            </div>
-            </nav>
-        </header>
-        <body>
-            <div class="flex flex-row justify-end items-center gap-x-5 bg-gray-300 rounded-full w-10/12 h-12 mx-auto my-10 px-10">
-                <DropDownList name="Display" :options="displayOptions" v-model:selectedOption="displayValue"/>
-                <DropDownList name="Sort By" :options="sortOptions" v-model:selectedOption="sortValue"/>
-            </div>
-            <div class="bg-red-300">
-                Test: {{ displayValue }} {{ sortValue }}
-            </div>
-        </body>    
+    <div class="flex flex-row justify-end items-center bg-ext-margins rounded-full w-10/12 h-12 mx-auto my-10 px-10">
+        <DropDownList name="Display" :options="displayOptions" v-model:selectedOption="displayValue"/>
+        <DropDownList name="Sort By" :options="sortOptions" v-model:selectedOption="sortValue"/>
     </div>
+
+    <div class="flex justify-center items-center ">
+        <ul class="overflow-y-auto flex-initial text-8xl">
+            <li>List element 1</li>
+            <li>List element 2</li>
+            <li>List element 3</li>
+            <li>List element 4</li>
+            <li>List element 5</li>
+            <li>List element 6</li>
+            <li>List element 7</li>
+            <li>List element 8</li>
+            <li>List element 9</li>
+        </ul>
     </div>
 </template>
 
@@ -42,6 +36,5 @@ const sortOptions = ref([
 
 const displayValue = ref("");
 const sortValue = ref("");
-
 
 </script>
