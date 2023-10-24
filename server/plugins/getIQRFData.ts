35e2bb2238b2ccb13a651c11a16923c501e00f52
Aleksander Łuczycki <aleksander.luczycki@op.pl>
2023-10-24 20:47:58 +0200

    fetching data using await Promise, see BIG TODO

--- server/plugins/getIQRFData.ts
+++ server/plugins/getIQRFData.ts
@@ -11,22 +11,58 @@ import { apiKey, gid, uid } from "../API_key"
 //the only option is to not verify their validity.
 env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
 
+/*BIG TODO
+You need to fetch as many new records as available.
+The reason is that the "uplc" fetch will produce at least 2 responses on the IQRF cloud - the confirmation and actual sensor data.
+Additionally, you need to take into accout all the other data that is sent to the server, like the "GW connected" one.
+If you were to fetch only one record every "dnld" fetch, then you would not be able to keep up - more records would be produced
+than you would be able to fetch, causing a catastrophic drift.
+Catch as many records as you can and then filter them
+*/
+
+
 export default defineNitroPlugin(async (nitroApp) => {
-    
     //i need to make the 2 fetches synchronously, hence the "await" on the first one
     //the upload request will not immediately return the data collected from the sensors - the content
     //of "result" is either "OK;" or "ERROR;"
     scheduleJob('*/20 * * * * *', async () => {
-        $fetch<string>(await constructURL("uplc"))
-        .then(async (res) => {
-            if (res !== "OK;") throw new Error("Iqrf cloud server error") //TODO better Error handling
-            await getRequestedSensorData(6, 1000*10).catch((err) => console.log) //6 retries, each waits 10s, for a total of 1min
-            return
-        })   
-        .catch((err) => {
+        var responseValid = await $fetch<string>(await constructURL("uplc"))
+        .then(res => {
+            return res === "OK;"
+        }).catch(err=>{
             console.log(err)
-            return  
-        }) 
+            return false
+        })
+
+        if (responseValid) await new Promise((resolve, reject) => {
+            const repLimit = 6
+            var reps = 0
+            const repDelay = 1000*10
+
+            const intervalID = setInterval(async () => {
+                $fetch<string>(await constructURL("dnld"))
+                .then(res => {
+                    const parsedData = parseRawServerData(res)
+                    if (parsedData !== null){
+                        clearInterval(intervalID)
+                        resolve(parsedData)
+                    }  //terminates the promise chain, goes to the if clause
+                    //++reps has to be the first operand (see AND docs)
+                    else if (++reps === repLimit && parsedData === null){
+                        clearInterval(intervalID)
+                        reject(`Fetching sensor data from server failed (${repLimit} retries)`)
+                    }
+                    return
+                })
+                .catch(err => console.log(err))
+
+            }, repDelay)
+        })
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err))
+
+        return
+        
     })
 })
 
@@ -66,35 +102,9 @@ async function constructURL(command: string): Promise<string> {
     return `${base}${parameter_part}&signature=${signature}`
 }
 
-
-async function getRequestedSensorData(retries: number, delay: number){
-    var repetitions = 0
-    var intervalID = setInterval(async () => {
-        $fetch<string>(await constructURL("dnld"))
-        .then((res) => {
-            const parsedData = parseRawServerData(res) 
-            if (parsedData === null) return
-            console.log(parsedData.readings)
-            clearInterval(intervalID)
-        })
-        .catch((err) => {
-            console.log(err)
-            return
-        })
-
-        if (++repetitions == retries){
-            clearInterval(intervalID)
-            //TODO: notify that, despite numerous retries, fetch has failed
-        }
-
-    }, delay)
-}
-
-
-/* the current regex does not capture the 'names' of readings - it does not capture the '01' in the temp 'frame' etc.
-If the order of sensors in fetched data does not change, then this is not an issue, but just to be sure 
-make it so the sensors can appear in any order and the readings still be properly captured */
-function parseRawServerData(rawData: string) {
+//TODO you need to consider the situation where the data fetched is not the sensor readings, for instance the GW connected signal.
+//you should either retry the request, without incrementing the retry counter, or fetch all new records and then filter them.
+function parseRawServerData(rawData: string): {datetime: Date; readings: number[]} | null {
     console.log(rawData)
     if (rawData === "0;;;\r\n") return null //no new data on the server
     
@@ -113,40 +123,41 @@ function parseRawServerData(rawData: string) {
     const datetime = new Date(arr[2])//arr1[2] gets the datetime field
     console.log(`${readings} @: ${datetime}`)
 
-    return {datetime, readings} //transform it before returning into the same format as the DB reqiures
+    return {datetime, readings} //TODO transform it before returning into the same format as the DB reqiures
 }
 
 
 //we need to filter out responses that we dont really caer about, like "GW connected" or "talked to a sensor"
-
 function filterSensorData(rawSensor: string): boolean {
     if (rawSensor.slice(0, 4) === "0000") return false //data sent by GW once it connects to the server
     // else if (rawSensor.length != 32 && rawSensor.length != 38) return false //every crucial-data-containing message
     //a propper sensor-message-data-thing has a sequence of bytes: XXXX.5E.81.(0140|0150)
     const filterRex = /(\d{4})(5E81)(0140|0150)(00)(?:[A-Z0-9]+)/g
     const matches = [...rawSensor.matchAll(filterRex)][0]
+    if (matches.length <= 0) return false //if matchAll does not find a match, it returns an empty list
 
     //TODO: at this point it should read the list of all available sensors from the db (0001, 0002, 0003, 0004)
     //and compare it with the \d{4}.
     if (matches[1] !== "0100") return false
-    if (matches[2] !== "5E81") return false
-    if (matches[3] !== "0140" && matches[3] !== "0150") return false
-    if (matches[4] !== "00") return false
+
+    //the below checks are useless - the string would just not match if any of those was false
+    //the regex would return an empty list, since we are trying to match some parts literally (5E81, etc)
+    // if (matches[2] !== "5E81") return false
+    // if (matches[3] !== "0140" && matches[3] !== "0150") return false
+    // if (matches[4] !== "00") return false
 
     return true 
 }
 
 function parseSensorData(rawSensor: string): number[] {
-
-    if (!filterSensorData(rawSensor)){
-        return []
-    }
+    if (!filterSensorData(rawSensor)) return []
 
     //Look up the sensor's docs - to get temperature from "raw" reading, multiply it by 0.0625 (to get C)
     //for humidity (in %) mult by 0.5, etc.
     const unitArray = [0.0625, 0.5, 1, 1]
     //split the data field into frames: metadata, temp, rh, co2, and optionally battery
     //TODO? Do i even need the named group? Consider another approach
+    //TODO this approach will only work if the order of sensor readings does not change.
     const re2 = /[A-F0-9]{16}01([A-F0-9]{4})80([A-F0-9]{2})02([A-F0-9]{4})(?:04(?<bat>[A-F0-9]{4}))?/g
     const arr2 = [...rawSensor.matchAll(re2)][0]
     const sensorReadings = arr2.slice(1,4) //TODO: i dont think you should rely on the position of readings in a list. Name all groups.
