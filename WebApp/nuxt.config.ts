f2c3ed2db48d757b632b00336f0ff3cdf8f3b880
Aleksander Łuczycki <aleksander.luczycki@op.pl>
2023-12-11 23:54:05 +0100

    attempt at u-auth

--- WebApp/nuxt.config.ts
+++ WebApp/nuxt.config.ts
@@ -1,11 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
-  devtools: { enabled: true },
-  typescript: {
-    typeCheck: true
-  },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+    ssr: false,
+    devtools: { enabled: true },
+    typescript: {
+        typeCheck: true
+    },
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@sidebase/nuxt-auth'
+    ],
+    auth: {
+        provider: {
+            type: 'local',
+            sessionDataType: {
+              	id: 'string',
+				name: 'string',
+				role: 'guest | admin' 
+            }
+        },
+        isEnabled: true,
+        baseURL: "/api/auth"
+    }
 })
---
+++ WebApp/pages/login.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <button @click="signIn({ username: 'jsmith', password: 'hunter2' })"></button>
+
+    <pre>{{ data }}</pre>
+
+</template>
+
+<script setup lang="ts">
+
+const {
+  status,
+  data,
+  token,
+  lastRefreshedAt,
+  getSession,
+  signUp,
+  signIn,
+  signOut,
+} = useAuth()
+
+</script>
